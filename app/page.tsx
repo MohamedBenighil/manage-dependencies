feec4bacc4cb55f5a1623b56c6f531f3a586cc89
Mohamed B. <mohamed.benighil07@gmail.com>
2024-11-06 11:59:21 +0100

    02. Paramétrage du projet (Installations)

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Home page</div>;
+  return (
+    <div>
+      <button className="btn">Button</button>
+
+      <button className="btn btn-accent">Accent</button>
+    </div>
+  );
 }
