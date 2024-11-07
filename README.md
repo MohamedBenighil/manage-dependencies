51a8103d95a8335425aa44effbbf89a492eef20d
Mohamed B. <mohamed.benighil07@gmail.com>
2024-11-07 08:47:09 +0100

    04.  Mise en place de l'authentification avec Clerk Auth

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ```bash
 - create new project
+- add clerk with local  "/signin" and "/signout" routes (instead of external routes like the default): use 2 env var
+    - Go hyperui : look for "auth form". Then redirect to the / after "/signin": use 2 env var. PS: it works without those env vars
 
 ```
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,29 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className=" flex flex-col justify-center items-center text-center gap-4 p-20">
-      <h1 className=" text-2xl md:text-4xl font-semibold">
-        Prenez le contrôle de vos finances
-      </h1>
-      <p className="text-gray-500 text-md md:text-sm">
-        Suivez vos budgets et vos dépences avec notre application intuitive
-      </p>
-      <div className="w-full flex justify-center gap-4">
-        <Link href="" className="btn btn-sm btn-outline md:btn-md btn-accent">
-          Se connecter
-        </Link>
-        <Link href="" className="btn btn-sm md:btn-md btn-accent">
-          S'inscrire
-        </Link>
+    <div>
+      <UserButton></UserButton>
+      <div className=" flex flex-col justify-center items-center text-center gap-4 p-20">
+        <h1 className=" text-2xl md:text-4xl font-semibold">
+          Prenez le contrôle de vos finances
+        </h1>
+        <p className="text-gray-500 text-md md:text-sm">
+          Suivez vos budgets et vos dépences avec notre application intuitive
+        </p>
+        <div className="w-full flex justify-center gap-4">
+          <Link
+            href="/sign-in"
+            className="btn btn-sm btn-outline md:btn-md btn-accent"
+          >
+            Se connecter
+          </Link>
+          <Link href="/sign-up" className="btn btn-sm md:btn-md btn-accent">
+            S'inscrire
+          </Link>
+        </div>
       </div>
     </div>
   );
